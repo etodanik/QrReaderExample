5afb11e40d0c4aef2860ccc1263d85d3fb4ab082
Danny Povolotski <dannypovolotski@gmail.com>
2023-09-25 21:01:03 +0300

    first commit

---
+++ Source/QrReaderExample/QrReaderExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QrReaderExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QRREADEREXAMPLE_API AQrReaderExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
